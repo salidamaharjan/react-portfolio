3789976143c1cf9ccb99389b41530143667a7e9c
salidamaharjan <salidamaharjan@outlook.com>
2024-03-29 21:23:32 -0500

    Create page and move AboutMe to Pages

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,5 +1,6 @@
 export default function AboutMe (){
     return <>
+    <h2>About Me</h2>
     <p>Welcome to my portfolio! As a full stack developer, I offer a unique perspective that merges technical proficiency. I embarked on a coding journey to leverage innovation for new apps.
 
     In this portfolio, you will find a collection of projects that reflect my diverse skill set, from developing applications to implementing solutions for data analysis. Through my coding endeavors, I aim to revolutionize healthcare delivery by creating user-friendly interfaces, streamlining processes, and ensuring data accuracy.
