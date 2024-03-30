f45a813257b412ffbe236b40565790e8224033a3
salidamaharjan <salidamaharjan@outlook.com>
2024-03-29 21:18:55 -0500

    creatE AboutMe component and install react-dom-router

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,8 @@
+export default function AboutMe (){
+    return <>
+    <p>Welcome to my portfolio! As a full stack developer, I offer a unique perspective that merges technical proficiency. I embarked on a coding journey to leverage innovation for new apps.
+
+    In this portfolio, you will find a collection of projects that reflect my diverse skill set, from developing applications to implementing solutions for data analysis. Through my coding endeavors, I aim to revolutionize healthcare delivery by creating user-friendly interfaces, streamlining processes, and ensuring data accuracy.
+    </p>
+    </>
+}
