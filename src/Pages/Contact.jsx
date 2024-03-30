e41932e6479a36147714a56b4c6b9364ff8b89da
salidamaharjan <salidamaharjan@outlook.com>
2024-03-30 08:48:32 -0500

    Label use and form created

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,11 @@
+import Label from "../components/Label"
 export default function Contact () {
     return <>
     <form>
         <h2>Contact</h2>
-        <label> Name:
-            <input type="text" />
-        </label> 
+        <Label labelName={"Name: "} />
+        <Label labelName={"Email Address: "}/>
+        <Label labelName={"Message: "} />
     </form> 
     </>
 }
--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,6 +1,6 @@
-export default function Label({labelName,inputType}){
-    return<>
-        <label htmlFor={inputType}> {labelName} 
-        </label>
-    </>
+export default function Label({ labelName, htmlFor }) {
+  return <> <label htmlFor={htmlFor}>{labelName}
+  </label>
+  <input className="border-2 border-gray-500"type="text"/>
+  </>
 }
