772314f708a691220589c2453ed8a58e60c93f48
salidamaharjan <salidamaharjan@outlook.com>
2024-03-30 12:43:24 -0500

    Footer component revise and css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLinkedin,
-  faTwitter,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="bg-gray-300 h-[100dvh] flex flex-col">
       <NavBar />
-      <Outlet />
-      <Footer className="gap-2 flex justify-center">
-        <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a href="https://twitter.com/?lang=en">
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        <a href="https://github.com/salidamaharjan">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </Footer>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,22 @@
-export default function Footer({className, children}) {
-    return<footer className={`${className}`}>{children}</footer>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faTwitter,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+  return (
+    <footer className={`flex gap-5 text-2xl p-4 justify-center items-end`}>
+      <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href="https://twitter.com/?lang=en">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <a href="https://github.com/salidamaharjan">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </footer>
+  );
 }
