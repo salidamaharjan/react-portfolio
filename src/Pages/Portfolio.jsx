f67624f520960204e3a5258193833338b80b9c14
salidamaharjan <salidamaharjan@outlook.com>
2024-03-30 13:22:06 -0500

    creat Tag component and simplify in Portfolio Page

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,3 +1,4 @@
+import Tag from "../components/Tag";
 import foodAndRecipe from "../assets/images/foodAndRecipe.png";
 export default function Portfolio() {
   return (
@@ -11,20 +12,12 @@ export default function Portfolio() {
           />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">Food and Recipes App</div>
-            <p className="text-gray-700 text-base">
-              
-            </p>
+            <p className="text-gray-700 text-base"></p>
           </div>
           <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Javascript, CSS, HTML
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Bulma
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Edamam
-            </span>
+            <Tag>#Javascript, CSS, HTML</Tag>
+            <Tag>#Bulma</Tag>
+            <Tag>#Edamam</Tag>
           </div>
         </div>
       </a>
