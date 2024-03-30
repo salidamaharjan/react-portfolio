5dfef6a2062ddfbc92d1d088aafcce4487f5324d
salidamaharjan <salidamaharjan@outlook.com>
2024-03-29 22:22:22 -0500

    Create Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutMe from "./Pages/AboutMe.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about",
+        element: <AboutMe />
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <RouterProvider router={router}/>
 );
