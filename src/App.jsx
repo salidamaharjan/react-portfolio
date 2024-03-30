337d65f9bfb0bb5f6f5b25ef6068a1e9df2c427c
salidamaharjan <salidamaharjan@outlook.com>
2024-03-30 12:17:26 -0500

    install fontawesome and use it in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faTwitter,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
-    <div className="bg-gray-300 h-[100dvh]">
+    <div className="bg-gray-300 h-[100dvh] flex flex-col">
       <NavBar />
       <Outlet />
-      <Footer />
+      <Footer className="gap-2 flex justify-center">
+        <a href="">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </Footer>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,3 @@
-export default function Footer() {
-    return<>footer</>
+export default function Footer({className, children}) {
+    return<footer className={`${className}`}>{children}</footer>
 }
