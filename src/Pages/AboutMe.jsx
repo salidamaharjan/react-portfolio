a41e1d71c6be8f40ea764b36ce3e628edb437049
salidamaharjan <salidamaharjan@outlook.com>
2024-04-01 16:17:55 -0500

    Update Resume and AboutMe component

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,25 +1,79 @@
 import salida from "../assets/images/salida.png";
 export default function AboutMe() {
   return (
-      <div className="flex justify-center min-h-[100dvh]">
-        <div className="flex max-w-5xl flex-col pt-8 gap-6">
-          <h2 className="font-bold text-2xl ">About Me</h2>
-          <img
-            className="w-[150px] h-[150px] rounded-full"
-            src={salida}
-            alt="ImageImage"
-          />
-          <p className="text-xl pt-4">
-            Welcome to my portfolio! As a full stack developer, I offer a unique
-            perspective that merges technical proficiency. I embarked on a
-            coding journey to leverage innovation for new apps. In this
-            portfolio, you will find a collection of projects that reflect my
-            diverse skill set, from developing applications to implementing
-            solutions for data analysis. Through my coding endeavors, I aim to
-            revolutionize creating user-friendly interfaces, streamlining
-            processes, and ensuring data accuracy.
+    <div className="flex justify-center min-h-[100dvh]">
+      <div className="flex max-w-5xl flex-col pt-8 gap-6">
+        <h2 className="font-bold text-3xl ">About Me</h2>
+        <img
+          className="w-[150px] h-[150px] rounded-full"
+          src={salida}
+          alt="ImageImage"
+        />
+        <p className="text-xl pt-4">
+          Welcome to my portfolio! As a full stack developer, I offer a unique
+          perspective that merges technical proficiency. I embarked on a coding
+          journey to leverage innovation for new apps. In this portfolio, you
+          will find a collection of projects that reflect my diverse skill set,
+          from developing applications to implementing solutions for data
+          analysis. Through my coding endeavors, I aim to revolutionize creating
+          user-friendly interfaces, streamlining processes, and ensuring data
+          accuracy.
+        </p>
+        <div>
+          <div className="text-2xl font-bold">Objective</div>
+          <p className="text-xl">
+            Motivated coding graduate with a passion for software development
+            and a desire to contribute to innovative projects. Eager to leverage
+            my newly acquired skills in web development to kickstart a rewarding
+            career in the tech industry.
           </p>
         </div>
+        <div>
+          <div className="text-2xl font-bold"> Skills</div>
+          <ul className="text-xl">
+            <li>Proficient in HTML, JavaScript, CSS</li>
+            <li>
+              Experience with frameworks/libraries such as React.js, Vue.js,
+              BULMA, TailWind, Chart.js
+            </li>
+            <li>
+              Familiar with backend development using Node.js and Express.js
+            </li>
+            <li>
+              Knowledge of database management systems including MongoDB and
+              MySQL
+            </li>
+            <li>
+              Knowledge of database management systems including MongoDB and
+              MySQL
+            </li>
+            <li>
+              Knowledge of database management systems including MongoDB and
+              MySQL
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="text-2xl font-bold">Projects</div>
+          <div className="text-xl">
+            Check out my{" "}
+            <a
+              className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
+              href="http://localhost:5173/portfolio"
+            >
+              Portfolio{" "}
+            </a>
+            and{" "}
+            <a
+              className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
+              href="https://github.com/salidamaharjan"
+            >
+              GitHub{" "}
+            </a>
+            for more details.
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -2,97 +2,20 @@ export default function Resume() {
   return (
     <div className="flex flex-col bg-gray-300  text-w pt-14 ml-40 gap-3">
       <div className="font-bold text-2xl">Resume</div>
+      <div>
+        Download <span className="underline underline-offset-4 hover:text-blue-500">Resume</span>
+      </div>
       <div className="flex flex-col gap-4">
-        <div>
-          <div className="text-lg font-bold">Salida Maharjan</div>
-          <div>
-            <div>
-              <a
-                className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
-                href="https://www.linkedin.com/in/salida-maharjan-6381b9173/"
-              >
-                LinkedIn
-              </a>
-            </div>
-            <div>
-              <a
-                className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
-                href="mailto:email@email.com"
-              >
-                Email
-              </a>
-            </div>
-            <div>
-              <a
-                className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
-                href="https://github.com/salidamaharjan"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="text-lg font-bold">Objective</div>
-          <p>
-            Motivated coding BootCamp graduate with a passion for software
-            development and a desire to contribute to innovative projects. Eager
-            to leverage my newly acquired skills in web development to kickstart
-            a rewarding career in the tech industry.
-          </p>
-        </div>
+        
         <div>
           <div className="text-lg font-bold">Education</div>
-          <li>Full Stack Web Development BootCamp</li>
-          <li>University Of Minnesota partnership with edX</li>
-          <li>Minneapolis, MN</li>
-          <li>Current</li>
+          <div>Full Stack Web Development BootCamp</div>
+          <div>University Of Minnesota partnership with edX</div>
+          <div>Minneapolis, MN</div>
+          <div>Current</div>
         </div>
-        <div>
-          <div className="text-lg font-bold"> Skills</div>
-          <ul>
-            <li>Proficient in HTML, JavaScript, CSS</li>
-            <li>
-              Experience with frameworks/libraries such as React.js, Vue.js,
-              BULMA, TailWind, Chart.js
-            </li>
-            <li>
-              Familiar with backend development using Node.js and Express.js
-            </li>
-            <li>
-              Knowledge of database management systems including MongoDB and
-              MySQL
-            </li>
-            <li>
-              Knowledge of database management systems including MongoDB and
-              MySQL
-            </li>
-            <li>
-              Knowledge of database management systems including MongoDB and
-              MySQL
-            </li>
-          </ul>
-        </div>
-        <div>
-          <div className="text-lg font-bold">Projects</div>
-          <div>
-            Check out my{" "}
-            <a
-              className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
-              href="http://localhost:5173/portfolio"
-            >
-              Portfolio{" "}
-            </a>
-            and{" "}
-            <a
-              className="underline underline-offset-2 hover:decoration-blue-600 hover:text-blue-600"
-              href="https://github.com/salidamaharjan"
-            >
-              GitHub{" "}
-            </a>
-            for more details.
-          </div>
-        </div>
+       
+        
       </div>
     </div>
   );
