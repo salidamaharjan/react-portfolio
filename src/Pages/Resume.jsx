bb46f4c04bd6136a741f231aa2aebb4415496e8e
salidamaharjan <salidamaharjan@outlook.com>
2024-04-01 10:12:37 -0500

    Add link to Resume

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -2,20 +2,25 @@ export default function Resume() {
   return (
     <div className="flex flex-col bg-gray-300  text-w pt-14 ml-40 gap-3">
       <div className="font-bold text-2xl">Resume</div>
-      <div>Download my Resume</div>
       <div className="flex flex-col gap-4">
         <div>
           <div className="text-lg font-bold">Salida Maharjan</div>
           <div>
-            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
-              LinkedIn
-            </a>
-            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
-              Email
-            </a>
-            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
-              Github
-            </a>
+            <div>
+              <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
+                LinkedIn
+              </a>
+            </div>
+            <div>
+              <a href="mailto:email@email.com">
+                Email
+              </a>
+            </div>
+            <div>
+              <a href="https://github.com/salidamaharjan">
+                Github
+              </a>
+            </div>
           </div>
         </div>
         <div className="text-lg font-bold">Objective</div>
