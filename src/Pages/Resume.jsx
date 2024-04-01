302ab83090650fe722906fdfad5ac16dc7ce51af
salidamaharjan <salidamaharjan@outlook.com>
2024-04-01 00:11:51 -0500

    updated css

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,12 +1,22 @@
 export default function Resume() {
   return (
-    <div className="flex flex-col pt-16 ml-60 gap-3 pt-4">
+    <div className="flex flex-col pt-14 ml-40 gap-3">
       <div className="font-bold text-2xl">Resume</div>
       <div>Download my Resume</div>
       <div className="flex flex-col gap-4">
         <div>
           <div className="text-lg font-bold">Salida Maharjan</div>
-          <div>LinkedIn, Email, GitHub</div>
+          <div>
+            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
+              LinkedIn
+            </a>
+            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
+              Email
+            </a>
+            <a href="https://www.linkedin.com/in/salida-maharjan-6381b9173/">
+              Github
+            </a>
+          </div>
         </div>
         <div className="text-lg font-bold">Objective</div>
         <div className="text-lg font-bold"> Skills</div>
