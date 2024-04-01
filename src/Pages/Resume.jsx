7d5939096042747d84c195508bbb1787d5dfc318
salidamaharjan <salidamaharjan@outlook.com>
2024-03-31 23:56:50 -0500

    Add Resume content

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,3 +1,13 @@
 export default function Resume() {
-    return <></>
+    return <div className="flex flex-col pt-16 ml-60 gap-3 pt-4">
+        <h1 className="font-bold text-2xl">Resume</h1>
+        <div>Download my Resume</div>
+        <div>
+        <h2 className="text-lg font-bold">Salida Maharjan</h2>
+        <div>LinkedIn, Email, GitHub</div>
+        <h2 className="text-lg font-bold">Objective</h2>
+        <h2 className="text-lg font-bold"> Skills</h2>
+        <h2 className="text-lg font-bold">Projects</h2>
+        </div>
+    </div>
 }
