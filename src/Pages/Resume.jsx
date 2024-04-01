8ad128b4a91471bc80b45a1d31731f9cc9c68337
salidamaharjan <salidamaharjan@outlook.com>
2024-04-01 00:03:47 -0500

    restructure the code in Resume component

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,13 +1,17 @@
 export default function Resume() {
-    return <div className="flex flex-col pt-16 ml-60 gap-3 pt-4">
-        <h1 className="font-bold text-2xl">Resume</h1>
-        <div>Download my Resume</div>
+  return (
+    <div className="flex flex-col pt-16 ml-60 gap-3 pt-4">
+      <div className="font-bold text-2xl">Resume</div>
+      <div>Download my Resume</div>
+      <div className="flex flex-col gap-4">
         <div>
-        <h2 className="text-lg font-bold">Salida Maharjan</h2>
-        <div>LinkedIn, Email, GitHub</div>
-        <h2 className="text-lg font-bold">Objective</h2>
-        <h2 className="text-lg font-bold"> Skills</h2>
-        <h2 className="text-lg font-bold">Projects</h2>
+          <div className="text-lg font-bold">Salida Maharjan</div>
+          <div>LinkedIn, Email, GitHub</div>
         </div>
+        <div className="text-lg font-bold">Objective</div>
+        <div className="text-lg font-bold"> Skills</div>
+        <div className="text-lg font-bold">Projects</div>
+      </div>
     </div>
+  );
 }
